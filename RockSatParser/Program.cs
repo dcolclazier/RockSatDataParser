43c034eeedef0f4401b1f7544ed2cb47934fd37a
David Colclazier <david@jdc.tech>
2016-06-04 02:28:19 -0600

    Data parsing bugs are fixed - all data is correct, including time stamps for geiger and accel.

--- RockSatParser/Program.cs
+++ RockSatParser/Program.cs
@@ -17,23 +17,28 @@ namespace RockSatParser
             //set up files with headers
             using (var tempGeiger = new FileStream("geiger.csv", FileMode.Create))
             {
-                var header = Encoding.UTF8.GetBytes("time(seconds),shielded, unshielded\n");
+                var header = Encoding.UTF8.GetBytes("time(millis),shielded, unshielded\n");
                 tempGeiger.Write(header,0,header.Length);
             }
             using (var tempAccel = new FileStream("accel.csv", FileMode.Create))
             {
-                var header = Encoding.UTF8.GetBytes("time(seconds),x,y,z\n");
+                var header = Encoding.UTF8.GetBytes("time(millis),x,y,z\n");
                 tempAccel.Write(header,0,header.Length);
             }
             using (var tempBNO = new FileStream("bno.csv", FileMode.Create))
             {
-                var header = Encoding.UTF8.GetBytes("time(seconds),gyro_x,gyro_y,gyro_z,accel_x,accel_y,accel_z,temp\n");
+                var header = Encoding.UTF8.GetBytes("time(millis),gyro_x,gyro_y,gyro_z,accel_x,accel_y,accel_z,temp\n");
                 tempBNO.Write(header, 0, header.Length);
             }
+            using (var temptimeSync = new FileStream("timeSync.csv", FileMode.Create))
+            {
+                var header = Encoding.UTF8.GetBytes("time(millis),gyro_x,gyro_y,gyro_z,accel_x,accel_y,accel_z,temp\n");
+                temptimeSync.Write(header, 0, header.Length);
+            }
 
-            Console.WriteLine("RockSat Data Parser, v0.1");
+            Console.WriteLine("RockSat Data Parser, v.9");
             Console.WriteLine("Make sure data file is in same directory as this utility.");
-            Console.WriteLine("Enter the full file name, including file extension. (For data.dat, enter \"data.dat\"");
+            Console.WriteLine("Enter the full file name, or drag and drop file onto window and press enter. (For data.dat, enter \"data.dat\"");
             var filename = Console.ReadLine();
 
             using (var fileStream = new FileStream(filename, FileMode.Open))
@@ -41,23 +46,25 @@ namespace RockSatParser
                 fileStream.Seek(0, SeekOrigin.Begin); // make sure we're at the beginning of the file
                 while (fileStream.Position != fileStream.Length)
                 {
-                    //first packet should be start packet (0xff)
+                    //first byte should be start packet (0xff)
                     if (fileStream.ReadByte() != 0xFF) {
                         Console.WriteLine("Error... expected data packet start, received something else...");
                         while (fileStream.ReadByte() != 0xFF && fileStream.Position != fileStream.Length) ;
                     }
                     //
 
+                    //second byte is packet type
                     var packetType = fileStream.ReadByte();
 
-                    var size_msb = fileStream.ReadByte();
-                    var size_lsb = fileStream.ReadByte();
+                    //3rd and 4th are size
+                    var sizeMsb = fileStream.ReadByte();
+                    var sizeLsb = fileStream.ReadByte();
+                    var dataSize = (sizeMsb << 8) + sizeLsb;
 
-                    var dataSize = (size_msb << 8) + size_lsb;
 
-                    var dataContainer = new byte[dataSize + 6]; //6 bytes for 2 time stamps.
+                    var dataContainer = new byte[dataSize]; //16 bytes for 2 time stamps.
 
-                    for (var i = 0; i < dataSize + 6; i++) {
+                    for (var i = 0; i < dataSize; i++) {
                         dataContainer[i] = (byte)fileStream.ReadByte();
                     }
 
@@ -71,64 +78,71 @@ namespace RockSatParser
 
         private static void parseData(int packetType,int dataSize, byte[] data)
         {
-            
+            var startMillisBinary = new[] {data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7] };
+            var startMillis = BitConverter.ToInt64(startMillisBinary,0);
 
-            var startHours = data[0];
-            var startMin = data[1];
-            var startSec = data[2];
-
-            var endSec = data[data.Length - 1];
-            var endMin = data[data.Length - 2];
-            var endHour = data[data.Length - 3];
-
-            var startSeconds = startHours*360 + startMin*60 + startSec;
-            var endSeconds = endHour*360 + endMin*60 + endSec;
-            var delta = Math.Abs(endSeconds - startSeconds);
-
-            var step = delta/dataSize;
-
-            var currentStep = startSeconds;
-
+            var startIndex = 8;
             switch (packetType) {
                 case 0x02: //BNO
 
                     Console.Write("BNO packet found. Processing...");
-                    using (var fileTest = new FileStream("bno.csv", FileMode.Append))
+                    using (var logFile = new FileStream("bno.csv", FileMode.Append))
                     {
-                        for (int i = 0; i < dataSize; i += 7)
-                        {
-                            
-                            var currentline = currentStep.ToString() + "," + data[i + 3] + "," + data[i + 4] +","+ data[i + 5] + "," + data[i + 6] + "," + data[i + 7] + "," + data[i + 8] + "," + data[i + 9] + "," + "\n";
-                            fileTest.Write(Encoding.UTF8.GetBytes(currentline), 0, currentline.Length);
-                            currentStep += step;
-                        }
+                        
+                        var currentline = startMillis + "," + data[startIndex] + "," + data[startIndex + 1] + "," + data[startIndex + 2] + "," + data[startIndex + 3] + "," + data[startIndex + 4] + "," + data[startIndex + 5] + "," + data[startIndex + 6] + "," + "\n";
+                        logFile.Write(Encoding.UTF8.GetBytes(currentline), 0, currentline.Length);
                     }
                     Console.WriteLine(" finished.");
                     break;
                 case 0x00: //GEIGER
                     Console.Write("Geiger packet found. Processing...");
-                    using (var fileTest = new FileStream("geiger.csv", FileMode.Append))
+                    using (var logFile = new FileStream("geiger.csv", FileMode.Append))
                     {
-                        for (int i = 0; i < dataSize; i+=2)
-                        {
-                            var currentline = currentStep.ToString() + "," + data[i + 3] + "," + data[i + 4] + "\n";
-                            fileTest.Write(Encoding.UTF8.GetBytes(currentline),0,currentline.Length);
-                            currentStep += step;
-                        }
+                        var currentline = startMillis + "," + data[startIndex] + "," + data[startIndex + 1] + "\n";
+                        logFile.Write(Encoding.UTF8.GetBytes(currentline), 0, currentline.Length);
+                        
                     }
                     Console.WriteLine(" finished.");
                     break;
                 case 0x01: //ACCELDUMP
 
+                    var endMillisBinary = new[] { data[data.Length - 8], data[data.Length - 7], data[data.Length - 6], data[data.Length - 5], data[data.Length - 4], data[data.Length - 3], data[data.Length - 2], data[data.Length - 1]};
+                    var endMillis = BitConverter.ToInt64(endMillisBinary, 0);
+                    var delta = Math.Abs(endMillis - startMillis);
+                    double step = (float)delta / dataSize;
+                    double currentStep = startMillis;
+
                     Console.Write("Acceleration packet found. Processing...");
-                    using (var fileTest = new FileStream("accel.csv", FileMode.Append))
+                    using (var logFile = new FileStream("accel.csv", FileMode.Append))
                     {
-                        for (int i = 0; i < dataSize; i += 3)
+                        for (var i = startIndex; i < dataSize - startIndex; i += 6) //startIndex = size of time data, there's time data at the end, and datasize includes size of both timestamps
                         {
-                            var currentline = currentStep.ToString() + "," + data[i + 3] + "," + data[i + 4] + "," + data[i + 5] + "\n";
-                            fileTest.Write(Encoding.UTF8.GetBytes(currentline), 0, currentline.Length);
+                            var currentX = (data[i] << 8) + data[i+1];
+                            var currentY = (data[i+2] << 8) + data[i+3];
+                            var currentZ = (data[i+4] << 8) + data[i+5];
+                            var currentline = currentStep + "," + currentX + "," + currentY + "," + currentZ + "\n";
+                            logFile.Write(Encoding.UTF8.GetBytes(currentline), 0, currentline.Length);
                             currentStep += step;
                         }
+                    }
+                    Console.WriteLine(" finished.");
+                    break;
+                case 0x03: // Time-sync
+                    Console.Write("Time-sync packet found. Processing...");
+                    using (var logFile = new FileStream("timeSync.csv", FileMode.Append))
+                    {
+                        var hours = data[0];
+                        var minutes = data[1];
+                        var seconds = data[2];
+
+                        var millis = new[] {data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10]};
+
+                        var e_hours = data[11];
+                        var e_mins = data[12];
+                        var e_sec = data[13];
+
+                        var currentline = hours + ":" + minutes + ":" + seconds + "," + BitConverter.ToInt64(millis,0) + "," + e_hours + ":" + e_mins + ":" + e_sec;
+                        logFile.Write(Encoding.UTF8.GetBytes(currentline), 0, currentline.Length);
                     }
                     Console.WriteLine(" finished.");
                     break;
